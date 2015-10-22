be8f015d7523f5af38f035118eced5defc47e5a2
Daniel Garcia-Briseno <dg.briseno@gmail.com>
2015-10-21 21:13:28 -0700

    sciviewer will never build. dump it

--- sciviewer/src/SVImage.h
+++
@@ -1,6 +0,0 @@
-typedef struct {
-	int Width, Height; // Image dimensions
-	int Format; // OpenGL pixel format
-	int BytesPerPixel; // Number of bytes per pixel
-	unsigned char *Data; // Pointer to pixel data
-} SVImage;
