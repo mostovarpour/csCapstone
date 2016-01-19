49da72eae650e08e47448982869b1a63bc812677
mostovarpour <matt.ostovarpour@gmail.com>
2016-01-19 12:48:50 -0700

    Working on getting it to compile on OSX. Should have no effect on linux and windows compatability.

--- src/headers/os.h
+++ src/headers/os.h
@@ -8,3 +8,16 @@
 typedef unsigned char bool;
 #define false 0
 #define true 1
+
+//These two ifdefs are for compiling on OSX
+#ifdef __APPLE__
+#include <OpenGL/gl.h>
+#else
+#include <GL/gl.h>
+#endif
+
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
