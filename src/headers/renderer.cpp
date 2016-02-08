b269bf08c2ac05b468340666b6ae73f589a94f78
= <=>
2016-02-07 22:16:42 -0700

    Added some scaffolding for work I'll do later

---
+++ src/headers/renderer.cpp
@@ -0,0 +1,3 @@
+#ifndef _RENDERER_H_
+#define _RENDERER_H_
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,7 @@ int _main_(int /*_argc*/, char** /*_argv*/)
 	bgfx::reset(width, height, reset);
 
 	// Enable debug text.
-	bgfx::setDebug(debug);
+	//bgfx::setDebug(debug);
 
 	// Set view 0 clear state.
 	bgfx::setViewClear(0
@@ -36,20 +36,6 @@ int _main_(int /*_argc*/, char** /*_argv*/)
 		// This dummy draw call is here to make sure that view 0 is cleared
 		// if no other draw calls are submitted to view 0.
 		bgfx::touch(0);
-
-		// Use debug font to print information about this example.
-		bgfx::dbgTextClear();
-                /*
-		 *bgfx::dbgTextImage(bx::uint16_max(width/2/8, 20)-20
-		 *                                 , bx::uint16_max(height/2/16, 6)-6
-		 *                                 , 40
-		 *                                 , 12
-		 *                                 , s_logo
-		 *                                 , 160
-		 *                                 );
-                 */
-		bgfx::dbgTextPrintf(0, 1, 0x4f, "bgfx/examples/00-helloworld");
-		bgfx::dbgTextPrintf(0, 2, 0x6f, "Description: Initialization and debug text.");
 
 		// Advance to next frame. Rendering thread will be kicked to
 		// process submitted rendering primitives.
