932237affe53a9c8bc641977e2c305389d948b11
Daniel Garcia Briseno <dg.briseno@gmail.com>
2016-04-07 18:06:30 -0700

    Removed references to glut

--- src/headers/toolbar.h
+++ src/headers/toolbar.h
@@ -1,7 +1,6 @@
 #ifndef __TOOLBAR_H_
 #define __TOOLBAR_H_
 #include "os.h"
-#include <GL/glut.h>
 #define ILUT_USE_OPENGL //Must be defined before calling the devIL headers
 #include <IL/il.h>
 #include <IL/ilu.h>
