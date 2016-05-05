3ac0c998eecb558c94ccfe2b5040ca7cdd400754
Daniel Garcia Briseno <dg.briseno@gmail.com>
2016-05-05 16:01:21 -0700

    Updated website

--- website/js/pageloader.js
+++ website/js/pageloader.js
@@ -1,38 +1,40 @@
 // This file will load in the parts of the page
-$(document).ready(function () {
-    getNav();
-    getHeader();
-    getTeam();
-    getProjectDetails();
-    getFooter();
-});
+
+
+var siteUrl = "site-chunks/";
+
+function getChunk(containerId, page) {
+    $.get(siteUrl + page + ".html").then(function (data) {
+        $(containerId).html(data);
+    })
+}
 
 function getNav() {
-    $.get('site-chunks/nav.html').then(function (data) {
+    $.get(siteUrl + 'nav.html').then(function (data) {
         $('#nav').html(data);
     })
 }
 
 function getTeam() {
-    $.get('site-chunks/team_info.html').then(function (data) {
+    $.get(siteUrl + 'team_content.html').then(function (data) {
         $('#team-content').html(data);
     })
 }
 
 function getProjectDetails() {
-    $.get('site-chunks/project_details.html').then(function (data) {
+    $.get(siteUrl + 'project_details.html').then(function (data) {
         $('#project-content').html(data);
     })
 }
 
 function getFooter() {
-    $.get('site-chunks/footer.html').then(function (data) {
+    $.get(siteUrl + 'footer.html').then(function (data) {
         $('#footer').html(data);
     })
 }
 
 function getHeader() {
-    $.get('site-chunks/header.html').then(function (data) {
+    $.get(siteUrl + 'header.html').then(function (data) {
         $('#content-header').html(data);
     })
 }
